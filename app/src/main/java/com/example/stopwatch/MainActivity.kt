ddee9cdb428e79711cfaa2bf4d050403807432ec
Greg <lupasco.grigori@gmail.com>
2022-07-27 23:15:23 +0300

    set up handler

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.stopwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import android.widget.TextView
 import java.util.*
@@ -17,30 +18,37 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         tvTimer = findViewById(R.id.tvTimer)
-
+        runTimer()
     }
 
     fun onClickStartTimer(view: View) {
         isRunning = true
     }
+
     fun onClickPauseTimer(view: View) {
         isRunning = false
     }
+
     fun onClickResetTimer(view: View) {
         isRunning = false
         seconds = 0
     }
 
-    fun runTimer() {
-        val hours = seconds / 3600
-        val minutes = (seconds % 3600) / 60
-        val second = seconds
+    private fun runTimer() {
+        val handler = Handler()
+        handler.post(object : Runnable {
+            override fun run() {
+                val hours = seconds / 3600
+                val minutes = (seconds % 3600) / 60
+                val second = seconds
 
-        val time = String.format(Locale.getDefault(), "%d:%02d:%02d", hours, minutes, second)
+                val time =
+                    String.format(Locale.getDefault(), "%d:%02d:%02d", hours, minutes, second)
+                tvTimer.text = time
+                if (isRunning) seconds++
 
-        tvTimer.text = time
-
-        if (isRunning) seconds++
-
+                handler.postDelayed(this, 1000)
+            }
+        })
     }
 }
